477ae33f2916de008a239103eda8ffd09842f411
prashakar <prashakarp@gmail.com>
2025-03-04 23:55:05 -0500

    test

--- data/data.ts
+++ data/data.ts
@@ -3,29 +3,25 @@ const data = {
     sitename: "Sandip Baikare Blog",
     sitetagline: "Something great is on the way! ✨ 🔥",
     siteurl: "https://github.com/baikaresandip",
-    sitelogo: "",
+    sitelogo: "public/My Girl Beauty Bar Square Logo (no BG).png",
     title: "Coming Soon!",
     description: "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
     newsletterheading: "Stay tuned for our live updates!",
     copyrightText: `Copyright © ${currentYear} | Design and Developed By &nbsp;<a target="_blank" class="no-underline md:underline" href="https://github.com/baikaresandip">Baikare Sandip</a>`,
     socialIconsHeading: "Follow Us 📣",
-    hideSubscribeForm: false, // make true to disable subscription form 
+    hideSubscribeForm: false, // make true to disable subscription form
     socialIcons: [
         {
             icon: "facebook",
-            link: "https://www.facebook.com/sandeep.baikare/",
+            link: "https://www.facebook.com/61573879460172",
         },
         {
-            icon: "twitter",
-            link: "https://twitter.com/BaikareSandeep",
-        },
-        {
-            icon: "linkedIn",
-            link: "https://www.linkedin.com/in/sandip-baikare/",
+            icon: "instagram",
+            link: "https://www.instagram.com/mygirlbeautybar/",
         },
     ],
     hide :{
-        subscribeForm: false, // make true to disable subscription form         
+        subscribeForm: false, // make true to disable subscription form
         header: false,
         content: false,
         footer: false,
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,11 @@ import data from "../../../data/data";
 import Navigation from "../Navigation/Navigation";
 
 function Header() {
-  const { sitename, sitetagline, siteurl } = data;
+  const { sitename, sitetagline, siteurl, sitelogo } = data;
   return (
-    <section className="container mx-auto p-4 text-center"></section>
+    <section className="container mx-auto p-4 text-center">
+      <img src={sitelogo} alt={sitename} className="w-10 h-10" />
+    </section>
   );
 }
 
